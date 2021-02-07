b9766e90e826520fd7315835203e22d6b4f6df78
hydff <dengff@w-ibeda.com>
2021-02-07 21:33:28 +0800

    add navie bayes algorithm

--- 0504_FeatureEngineering.py
+++ 0504_FeatureEngineering.py
@@ -18,3 +18,27 @@ data = [
 vec = DictVectorizer(sparse=False, dtype=int)
 vec_trans = vec.fit_transform(data)
 print(vec_trans)
+vec_name = vec.get_feature_names()
+print(vec_name)
+vec = DictVectorizer(sparse=True, dtype=int)
+print(vec.fit_transform(data))
+
+sample = ['problem of evil',
+          'evil queen',
+          'horizon problem']
+from sklearn.feature_extraction.text import CountVectorizer
+vec_sample = CountVectorizer()
+X = vec_sample.fit_transform(sample)
+print("X is {}".format(X))
+import pandas as pd
+pd_data = pd.DataFrame(X.toarray(), columns=vec_sample.get_feature_names())
+print('pd_data is \n{}'.format(pd_data))
+from sklearn.feature_extraction.text import TfidfVectorizer
+vec_tfid = TfidfVectorizer()
+X_tfid = vec_tfid.fit_transform(sample)
+pd_tfid = pd.DataFrame(X_tfid.toarray(), columns=vec_tfid.get_feature_names())
+print('pd_tfid is \n{}'.format(pd_tfid))
+
+# 
+
+
